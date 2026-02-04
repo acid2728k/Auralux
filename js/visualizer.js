d91640b33ab0d780a068d4ee45da2f26563b538d
acid2728k <acid2728k@users.noreply.github.com>
2026-02-04 15:11:53 +0300

    Fix Three.js module imports with importmap

--- js/visualizer.js
+++ js/visualizer.js
@@ -3,10 +3,10 @@
  * Deep 3D scene with wireframe geometry, glow, and particles
  */
 
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { EffectComposer } from 'https://unpkg.com/three@0.160.0/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'https://unpkg.com/three@0.160.0/examples/jsm/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from 'https://unpkg.com/three@0.160.0/examples/jsm/postprocessing/UnrealBloomPass.js';
+import * as THREE from 'three';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 
 export class Visualizer {
     constructor(container) {
